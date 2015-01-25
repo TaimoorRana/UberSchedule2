759ad7d001d5e951295710bee17a6e8536ebfbb1
Taimoor <taimoorrana1@hotmail.com>
2015-01-24 20:21:37 -0500

    html text update

--- UberScheduleApp/app/controllers/demo_controller.rb
+++ UberScheduleApp/app/controllers/demo_controller.rb
@@ -1,9 +1,14 @@
 class DemoController < ApplicationController
   layout false;
   def index
+    
   end
   
   def hello
     @array = [1,2,3,4,5]
+  end
+  
+  def schedule
+    @courses = Course.all
   end
 end
