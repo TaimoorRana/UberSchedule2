82ac2d69bc29f14260f1c6390712b5704499b56c
Taimoor <taimoorrana1@hotmail.com>
2015-02-27 23:22:20 -0500

    Changed migration file according to specifications

--- UberScheduleApp/db/migrate/20150125153508_alter_courses.rb
+++ UberScheduleApp/db/migrate/20150125153508_alter_courses.rb
@@ -1,11 +1,15 @@
 class AlterCourses < ActiveRecord::Migration
   def up
-    add_column("courses","time",:string, :limit => 15 ,:default => "12h00 - 13h00")
-    add_column("courses","location",:string, :limit => 10, :default => "H-535")
+    add_column("courses","course_code",:string, :limit => 15 ,:default => "course code")
+    add_column("courses","name",:string ,:default => "course name")
+    add_column("courses","credits",:float, :limit => 2 ,:default => "3")
+    add_column("courses","description",:string,:default => "course description")
   end
   
   def down
-    remove_column("courses","location")
-    remove_column("courses","time")
+    remove_column("courses","course_code")
+    remove_column("courses","name")
+    remove_column("courses","credits")
+    remove_column("courses","description")
   end
 end
