d2e23bae1a53f5c7a27136582287a4190296a562
Taimoor <taimoorrana1@hotmail.com>
2015-02-27 23:22:35 -0500

    Changed migration file according to specifications

--- UberScheduleApp/db/migrate/20150124225103_create_courses.rb
+++ UberScheduleApp/db/migrate/20150124225103_create_courses.rb
@@ -1,8 +1,6 @@
 class CreateCourses < ActiveRecord::Migration
   def change
     create_table :courses do |t|
-      t.string "name"
-      t.string "course_number"
       t.timestamps null: false
     end
   end
